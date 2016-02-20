30f501bbc4dad4309afbe44ee8049ed33a53b1d2
djmthe1 <mcgraththe1@gmail.com>
2016-02-20 01:06:14 -0400

    creacion de la clase Piezas

---
+++ BLL/Piezas.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    class Piezas : ClaseMaestra
+    {
+        public int PiezaId { set; get; }
+        public string Descripcion { set; get; }
+        public ConexionDb conexion = new ConexionDb();
+
+        public Piezas(int piezaId, string descripcion)
+        {
+            this.PiezaId = piezaId;
+            this.Descripcion = descripcion;
+        }
+
+        public Piezas()
+        {
+        }
+
+        public override bool Insertar()
+        {
+            bool retorno = false;
+            try
+            {
+                this.PiezaId = (int)conexion.ObtenerValor(String.Format("Insert Into Piezas (Descripcion) Values('{0}') Select @@Identity", this.Descripcion));
+                retorno = this.PiezaId > 0;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Editar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Update Piezas set Descripcion='{0}' where PiezaId={2}", this.Descripcion, this.PiezaId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Eliminar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Delete From Piezas where PiezaId={0}", this.PiezaId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            DataTable dt = new DataTable();
+
+            dt = conexion.ObtenerDatos("Select * From Piezas Where PiezaId=" + IdBuscado);
+            if (dt.Rows.Count > 0)
+            {
+                this.PiezaId = (int)dt.Rows[0]["PiezaId"];
+                this.Descripcion = dt.Rows[0]["Descripcion"].ToString();
+            }
+            return dt.Rows.Count > 0;
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            string ordenFinal = "";
+            if (!Orden.Equals(""))
+                ordenFinal = " orden by  " + Orden;
+            return conexion.ObtenerDatos(string.Format("select " + Campos + " from Piezas where " + Condicion + Orden));
+
+        }
+    }
+}
--- DAL/Script.sql
+++ DAL/Script.sql
@@ -7,3 +7,48 @@
 
 Go
 
+Create Table Piezas(
+	PiezaId int Identity(1,1) Primary Key,
+	Descripcion varchar(50),
+)
+
+Go
+
+Create Table Marcas(
+	MarcaId int Identity(1,1) Primary Key,
+	Descripcion varchar(50),
+)
+
+Go
+
+Create Table Clientes(
+	ClienteId int Identity(1,1) Primary Key,
+	Nombre varchar(40),
+	Telefono varchar(15),
+	Celular varchar(15),
+	Direccion varchar(30),
+	Cedula varchar(20),
+	Sexo varchar(10)
+)
+
+Go
+
+create table Entradas(
+	EntradaNo int identity primary key,
+	Fecha date,
+	FechaEntrega date,
+	Descripcion varchar (100),
+	MontoAPagar int,
+	RecibidoPor varchar(20)
+)
+
+Go
+
+create table Facturas(
+	FacturaNo int identity primary key,
+	Fecha date,
+	MontoAPagar int,
+	AutorizadoPor varchar(20)
+)
+
+Go
